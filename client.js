3f533114e3a6a93d195d0be8c91a7b7acecd6107
WSINTRA <will.simpson85@gmail.com>
2019-12-09 17:35:36 -0500

    Created custom native module for changing surface shape

--- client.js
+++ client.js
@@ -1,23 +1,40 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance, Module, Surface } from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
+    nativeModules: [
+    new surfaceModule(),
+    ],
     ...options,
   });
-
+  surface = r360.getDefaultSurface();
   // Render your app content to the default cylinder surface
+  //default is 1000 * 600
   r360.renderToSurface(
     r360.createRoot('surfacesExample', { /* initial props */ }),
-    r360.getDefaultSurface()
+    surface
   );
 
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+}
+
+class surfaceModule extends Module {
+  constructor() {
+    super('surfaceModule');
+  }
+  resizeSurface(width,height){
+    surface.resize(width, height);
+  }
+  changeSurfaceType(type){
+    type === "Flat" ? surface.setShape(Surface.SurfaceShape.Flat) :
+    surface.setShape(Surface.SurfaceShape.Cylinder)
+  }
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -2,34 +2,69 @@ import React from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  NativeModules,
   Text,
   View,
+  VrButton
 } from 'react-360';
 
+const surfaceModule = NativeModules.surfaceModule;
+
 export default class surfacesExample extends React.Component {
+
+  state = {
+    width: 1000,
+    height: 600,
+  }
+
+  changeSurface(width, height){
+    surfaceModule.resizeSurface(width, height)
+    this.setState({
+      width: width,
+      height: height
+    })
+  }
+
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
+         <View style={[styles.panel, {width: this.state.width, height: this.state.height}]}>
+        <VrButton style={styles.greetingBox } 
+        onClick={()=> this.changeSurface(500, 300)}>
+          <Text>Change Dimensions.</Text>
+        </VrButton>
+
+        <VrButton style={styles.greetingBox}
+        onClick={()=> surfaceModule.changeSurfaceType("Flat")}>
+          <Text>Flat</Text>
+        </VrButton>
+
+        <VrButton style={styles.greetingBox}
+        onClick={()=> surfaceModule.changeSurfaceType("Cylinder")}>
+          <Text>Cylinder</Text>
+        </VrButton>
+
+        <VrButton style={styles.greetingBox}
+        onClick={()=> this.changeSurface(1000, 600)}>
+          <Text>Reset</Text>
+        </VrButton>
+
+        <VrButton style={styles.greetingBox}>
+          <Text>Destroy</Text>
+        </VrButton>
       </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
+panel: {
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     justifyContent: 'center',
     alignItems: 'center',
   },
   greetingBox: {
+    width: 200,
+    height: 60,
     padding: 20,
     backgroundColor: '#000000',
     borderColor: '#639dda',
@@ -38,6 +73,13 @@ const styles = StyleSheet.create({
   greeting: {
     fontSize: 30,
   },
+  buttonPanel: {
+    width: 300,
+    height: 300,
+    backgroundColor: 'rgb(255,127,80)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 AppRegistry.registerComponent('surfacesExample', () => surfacesExample);
